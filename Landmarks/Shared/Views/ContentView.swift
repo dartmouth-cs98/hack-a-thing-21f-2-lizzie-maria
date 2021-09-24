30571aa8a6c787d863df9bc18e76ca8a2f3a167f
Maria Mora <maria.p.mora.bolanos.22@dartmouth.edu>
2021-09-24 19:16:35 -0400

    SwiftUI Essentials

---
+++ Landmarks/Shared/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Maria Paula Mora on 9/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
